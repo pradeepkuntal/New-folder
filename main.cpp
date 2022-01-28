f08fe023e75892308d6a18258b3acfc016fee704
pradeep kuntal <pradeepkuntal22@gmail.com>
2022-01-28 20:59:36 +0530

    first commit

---
+++ main.cpp
@@ -0,0 +1,20 @@
+/////
+
+#include<bits/stdc++.h>
+
+using namespace std;
+
+
+
+
+int main()
+{
+
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    cout<<"Hello world"<<endl;
+ 
+  
+
+    return 0;
+}
